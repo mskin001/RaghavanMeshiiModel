1907cb957aedf6eea3ff86b8c0fee14bf6b744d1
Miles Skinner <skinner.miles@gmail.com>
2018-11-28 14:02:28 -0700

    Update RaghavanMeshiiModel.m

--- RaghavanMeshiiModel.m
+++ RaghavanMeshiiModel.m
@@ -1,7 +1,23 @@
-%% Define rectangular test specimens-------------
+%% Loading conditions
+temp = 295; %K
+sig.app = 15; %[MPa] applied stress at temperature = temp
+
+%% Define rectangular test specimens
 length = 165; %mm
 width = 12.7; %mm
 thickness = 8; %mm
 
+
 %% Define material properties
 [resin, zero, ten, ninty, fourtyfive, shear] = generateMaterialPropertyTables;
+
+%% Calculate initial loading conditions
+e.in = sig.app / resin.inst(1); % e.in = initial strain
+
+% sig.int = internal stress. currently this value is under the initial
+% loading conditions
+sig.int = e.in * resin.rubber(1); 
+
+% sig.eff = effective stress. currently this is under the initial loading
+% conditions
+sig.eff = sig.app - sig.int; 
