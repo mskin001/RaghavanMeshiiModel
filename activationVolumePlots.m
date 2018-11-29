cb6d139ef53b8381501c986b021bbff7c4403208
Miles Skinner <skinner.miles@gmail.com>
2018-11-29 14:25:01 -0700

    Update activationVolumePlots.m

--- activationVolumePlots.m
+++ activationVolumePlots.m
@@ -2,24 +2,51 @@
 
 stress = linspace(10^4, 10^8, 10000);
 
-%% Evaulate resin data
+%% Evaulate equation 14 with C and D data from table 7
 
-nu = zeros(length(stress), length(resin.temp));
+nu = zeros(length(stress), 3);
+nuR = nu;
+nuT = nu;
+nuN = nu;
 
-cR = mean([resin.cu resin.cl]);
-cZ = mean([zero.cu, zero.cl]);
-cT = mean([ten.cu, ten.cl]);
-cN = mean([ninty.cu, ninty.cl]);
+cRmean = mean([resin.cu resin.cl]);
+cTmean = mean([ten.cu, ten.cl]);
+cNmean = mean([ninty.cu, ninty.cl]);
 
-for k = 1:length(resin.temp)
-  nuR(k) = resin.temp(k) .* cR .* stress .* resin.d;
-  nuZ(k) = zero.temp(k) .* cZ .* stress .* zero.d;
-  nuT(k) = ten.temp(k) .* cT .* stress .* ten.d;
-  nuN(k) = ninty.temp(k) .* cN .* stress .* ninty.d;
+cR = [resin.cl, cRmean, resin.cu];
+cT = [ten.cl, cTmean, ten.cu];
+cN = [ninty.cl, cNmean, ninty.cu];
+
+for k = 1:3
+  nuR(:,k) = cR(k) .* stress .^ -resin.d;
+  nuT(:,k) = cT(k) .* stress .^ -ten.d;
+  nuN(:,k) = cN(k) .* stress .^ -ninty.d;
 
 end
 [rows,cols] = size(nuR);
 
-for k = 1:cols
-  plot(nu(:,k),stress)
-end
+% for k = 1:cols
+%     figure(1)
+%     loglog(stress, nuR(:,k))
+%     hold on
+%
+%
+% end
+
+figure(1)
+loglog(stress,nuR)
+xlabel('Effective Stress: Pa')
+ylabel('Activation volume/T : [m^3K^-1]')
+legend('Lower Bound', 'Mean C Value', 'Upper Bound')
+
+figure(2)
+loglog(stress,nuT)
+xlabel('Effective Stress: Pa')
+ylabel('Activation volume/T : [m^3K^-1]')
+legend('Lower Bound', 'Mean C Value', 'Upper Bound')
+
+figure(3)
+loglog(stress,nuN)
+xlabel('Effective Stress: Pa')
+ylabel('Activation volume/T : [m^3K^-1]')
+legend('Lower Bound', 'Mean C Value', 'Upper Bound')
