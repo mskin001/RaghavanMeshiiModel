cde291d5123d2e35ed08a5a907b592f11c50439b
Miles Skinner <skinner.miles@gmail.com>
2018-11-29 15:03:26 -0700

    Update activationVolumePlots.m

--- activationVolumePlots.m
+++ activationVolumePlots.m
@@ -1,6 +1,6 @@
 [resin, zero, ten, ninty, fourtyfive, shear] = generateMaterialPropertyTables;
 
-stress = linspace(10^4, 10^8, 10000);
+stress = linspace(1e4, 1.4e7, 10000);
 
 %% Evaulate equation 14 with C and D data from table 7
 
@@ -38,15 +38,18 @@ loglog(stress,nuR)
 xlabel('Effective Stress: Pa')
 ylabel('Activation volume/T : [m^3K^-1]')
 legend('Lower Bound', 'Mean C Value', 'Upper Bound')
+text(1.4e4, 1e-29, 'Epoxy only')
 
 figure(2)
 loglog(stress,nuT)
 xlabel('Effective Stress: Pa')
 ylabel('Activation volume/T : [m^3K^-1]')
 legend('Lower Bound', 'Mean C Value', 'Upper Bound')
+text(1.4e4, 1.8e-29, '[10]_8')
 
 figure(3)
 loglog(stress,nuN)
 xlabel('Effective Stress: Pa')
 ylabel('Activation volume/T : [m^3K^-1]')
 legend('Lower Bound', 'Mean C Value', 'Upper Bound')
+text(1.4e4, 1e-29, '[90]_1_6')
